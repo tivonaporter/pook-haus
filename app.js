6fe2be51286aa045a229394caa2cd25f0073a809
Devon Tivona <devon@tivona.me>
2018-02-26 22:32:04 -0700

    Add support for Google Actions SDK and remove basestations

--- app.js
+++ app.js
@@ -21,10 +21,13 @@ const Node = bookshelf.Model.extend({
   tableName: 'nodes'
 })
 
-const Basestation = bookshelf.Model.extend({
-  tableName: 'basestations',
-  nodes: function() { return this.hasMany(Node) }
-})
+let sendCommand = function(command) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(command));
+    }
+  })
+}
 
 app.use(serveStatic('public', { 'index': false }))
 app.use(bodyParser.json())
@@ -32,21 +35,64 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) =>
-  Basestation.fetchAll({withRelated: ['nodes']}).then((results) => {
-    console.log(results.toJSON())
-    res.render('index', { basestations: results.toJSON() })
+app.post('/', function(req, res) {
+  console.log(req.body);
+  req.body.inputs.map(function(input) {
+    switch (input.intent) {
+      case "action.devices.SYNC":
+        Node.where("name", "!=", "null").fetchAll().then((results) => {
+          devices = results.models.map(function(node) {
+            return {
+              "id": node.get("id"),
+              "type": "action.devices.types.LIGHT",
+              "traits": [ "action.devices.traits.OnOff" ],
+              "name": { "name": node.get("name") },
+              "willReportState": false,
+            }
+          });
+          res.send({
+            "requestId": req.body.requestId,
+            "payload": { "devices": devices }
+          })
+        })
+        break
+      case "action.devices.EXECUTE":
+        let commands = input.payload.commands.map(function(command) {
+          let devices = command.devices.map(function(device) {
+            command.execution.map(function(execution) {
+              if (execution.command == "action.devices.commands.OnOff") {
+                sendCommand({
+                  "id" : device.id,
+                  "command" : execution.params.on ? "on" : "off"
+                })
+              }
+            })
+            return device.id
+          })
+          return {
+            "devices" : devices,
+            "status" : "SUCCESS"
+          }
+        })
+        res.send({
+          "requestId": req.body.requestId,
+          "payload": { "commands": commands }
+        })
+    }
   })
-)
+})
+
+app.get('/', function(req, res) {
+  console.log(req.body)
+  Node.fetchAll().then((results) => {
+    console.log(results.toJSON())
+    res.render('index', { "nodes" : results.toJSON() })
+  })
+})
 
 app.post('/command', function(req, res) {
   console.log("POST: /command")
-  console.log(req.body)
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(req.body));
-    }
-  })
+  sendCommand(req.body)
   res.sendStatus(200)
 })
 
@@ -59,13 +105,18 @@ app.post('/sync', function(req, res) {
       Node.forge({id: d}).upsert({basestation_id: b.id})
     }));
   }).then(() => res.sendStatus(200))
-});
+})
 
 wss.on('connection', function connection(ws, req) {
-  const location = url.parse(req.url, true);
   ws.on('message', function incoming(message) {
     console.log('Received: %s', message);
-  });
-});
+    j = JSON.parse(message)
+    if (j.command == "register") {
+      Node.forge({id: j.mac}).upsert().then(function(n) {
+        console.log(n);
+      })
+    }
+  })
+})
 
 server.listen(4236, () => console.log('Example app listening on port 4236!'))
